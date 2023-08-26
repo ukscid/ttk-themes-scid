76ccd28e8664457da81e130ff457c465309d3b21
Uwe <Uwe>
2023-08-26 13:32:13 +0200

    script build.sh to generate zip-file for versions

---
+++ build.sh
@@ -0,0 +1,17 @@
+echo "Enter version number:"
+read version
+mkdir ScidDarkTheme
+cp -p tclthemes/pkgIndexDark.tcl ScidDarkTheme/pkgIndex.tcl
+cp -p tclthemes/readme.md ScidDarkTheme
+cp -p LICENSE ScidDarkTheme
+cp -p -R tclthemes/sciddark ScidDarkTheme
+cp -p -R iconthemes/darkicons ScidDarkTheme
+zip -r sciddarktheme_V$version.zip ScidDarkTheme
+rm -rf ScidDarkTheme
+mkdir ScidLightTheme
+cp -p tclthemes/pkgIndexLight.tcl ScidLightTheme/pkgIndex.tcl
+cp -p tclthemes/readme.md ScidLightTheme
+cp -p LICENSE ScidLightTheme
+cp -p -R tclthemes/scidlight ScidLightTheme
+zip -r scidlighttheme_V$version.zip ScidLightTheme
+rm -rf ScidLightTheme
