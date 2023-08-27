f6b8f2565e76ffaefc5a5191b0c1494fa73104d7
Uwe <Uwe>
2023-08-27 16:27:50 +0200

    Rename Releasefiles

--- build.sh
+++ build.sh
@@ -6,15 +6,15 @@ cp -p tclthemes/readme.md ScidDarkTheme
 cp -p LICENSE ScidDarkTheme
 cp -p -R tclthemes/sciddark ScidDarkTheme
 cp -p -R iconthemes/darkicons ScidDarkTheme
-zip -r sciddarktheme_V$version.zip ScidDarkTheme
+zip -r ScidDarkTheme_V$version.zip ScidDarkTheme
 rm -rf ScidDarkTheme
 mkdir ScidLightTheme
 cp -p tclthemes/pkgIndexLight.tcl ScidLightTheme/pkgIndex.tcl
 cp -p tclthemes/readme.md ScidLightTheme
 cp -p LICENSE ScidLightTheme
 cp -p -R tclthemes/scidlight ScidLightTheme
-zip -r scidlighttheme_V$version.zip ScidLightTheme
+zip -r ScidLightTheme_V$version.zip ScidLightTheme
 rm -rf ScidLightTheme
 cd iconthemes
-zip -r ../scidDarkIcons_V$version.zip darkicons readme.md
+zip -r ../ScidDarkIcons_V$version.zip darkicons readme.md
 cd ..
