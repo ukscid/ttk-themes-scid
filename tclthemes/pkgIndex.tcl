125b5c945776a8d75a3a6bbedbe89a99f99738cf
Uwe <Uwe>
2023-08-27 16:10:11 +0200

    loading icons included

--- tclthemes/pkgIndex.tcl
+++ tclthemes/pkgIndex.tcl
@@ -1,4 +1,16 @@
 set themesdir [file join [pwd] [file dirname [info script]]]
 package provide ttk-themes 1.0
+
+proc loadThemeIcons { dir } {
+    #Load all icons for darkthemes
+    set dname [file join [file dirname [info script]] $dir]
+    foreach {fname} [glob -directory $dname *.gif *.png] {
+        set iname [string range [file tail $fname] 0 end-4]
+        catch { image delete $iname }
+        image create photo $iname -file $fname
+    }
+}
+
+catch { loadThemeIcons darkicons }
 source [file join $themesdir scidlight scidlight.tcl]
 source [file join $themesdir sciddark sciddark.tcl]
