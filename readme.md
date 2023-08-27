7ccb8f9f2969813a7c1a5c1a7042d3e75fdd6079
Uwe <Uwe>
2023-08-27 16:05:28 +0200

    new readme.md

---
+++ readme.md
@@ -0,0 +1,6 @@
+Scid TCL and Icon Themes
+========================
+
+TCL-Themes and Icon theme packs for Chess Database [SCID](https://sourceforge.net/projects/scid).
+
+See installation guide in the packages.
--- tclthemes/pkgIndexDark.tcl
+++ tclthemes/pkgIndexDark.tcl
@@ -1,9 +1,9 @@
 set themesdir [file join [pwd] [file dirname [info script]]]
 package provide ttk-themes 1.0
 
-proc loadThemeIcons { } {
+proc loadThemeIcons { dir } {
     #Load all icons for darkthemes
-    set dname [file join [file dirname [info script]] darkicons]
+    set dname [file join [file dirname [info script]] $dir]
     foreach {fname} [glob -directory $dname *.gif *.png] {
         set iname [string range [file tail $fname] 0 end-4]
         catch { image delete $iname }
@@ -11,5 +11,5 @@ proc loadThemeIcons { } {
     }
 }
 
-catch { loadThemeIcons }
+catch { loadThemeIcons darkicons }
 source [file join $themesdir sciddark sciddark.tcl]
